371cbe358c8425cfe87169d6e225aa74b865dbdd
Leonardo Villarreal <leonardovmdc@gmail.com>
2021-07-21 13:33:46 -0500

    href detail links added

--- web/routes/products.js
+++ web/routes/products.js
@@ -7,7 +7,6 @@ const productsFilePath = path.join(__dirname, '../data/allProducts.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 
-
 /* GET all products page. */
 router.get('/', function(req, res, next) {
   res.render('products', 
@@ -16,5 +15,13 @@ router.get('/', function(req, res, next) {
   products: products
  });
 });
+
+
+router.get('/detail/:category/:id', (req, res, next) =>{
+  const category = req.params.category;
+  const id = req.params.id;
+
+  res.send("" + category + " " + id)
+})
 
 module.exports = router;
