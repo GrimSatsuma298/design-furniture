05320f76f1f587af6869cac9b252cdd8d123370c
Leonardo Villarreal <leonardovmdc@gmail.com>
2021-07-21 13:09:56 -0500

    Contact page added

--- web/routes/index.js
+++ web/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'HOME LEO DESIGN&FURNITURE', cssFile: 'home' });
 });
 
+router.get('/contacto', (req, res, next)=>{
+  res.render('contact',{ title: 'CONTACT LEO DESIGN&FURNITURE', cssFile: 'contact' })
+})
+
 module.exports = router;
